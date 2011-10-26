7e115afc5116fd3e44cf90b4d3e71e3d3682fe02
Anthony Jaguenaud <capello@jaguenaud.org>
2011-10-27 00:03:38 +0200

    Start first skeleton software
    To implements library and test it.
    
    SimpleTextFile software creates a share resource, and reread it. Should
    be the same.
    
    ShareDeamon is the deamon implementation to able administrator to manage
    is network.
    
    Signed-off-by: Anthony Jaguenaud <capello@jaguenaud.org>

---
+++ Tests/SimpleTextFile/source/simpleTextFile.cpp
@@ -0,0 +1,7 @@
+/// \file
+/// \brief This file implements a simble text file test.
+/// \author Anthony Jaguenaud
+/// \version v0.1
+///
+/// The objective of this test is to create a share ressource with a simble text file.
+/// And then, show that cache as been correctly updated.
---
+++ Tools/ShareDeamon/source/ShareDeamon.cpp
@@ -0,0 +1,7 @@
+/// \file
+/// \brief Deamon implementation.
+/// \author Anthony Jaguenaud
+/// \version v0.1
+///
+/// This file implements the deamon to share disc space and only one network manager for multiple 
+/// softwares and users.
